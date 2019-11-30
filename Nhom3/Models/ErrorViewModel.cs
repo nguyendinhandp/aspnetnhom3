977888796ca647782365d78b0c20acc4f4599ff5
Duy <nguyenhoangduy1997@gmail.com>
2019-11-30 10:04:21 +0800

    reup source code net core 2.1

---
+++ Nhom3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nhom3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
