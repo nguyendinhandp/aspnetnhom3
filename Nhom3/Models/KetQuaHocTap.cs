847c8a58292a4cc09d94c0a0ccac58fc086e0f1a
mn <45994192+mnnguyen1998@users.noreply.github.com>
2019-11-30 10:34:10 +0700

    Update KetQuaHocTap.cs

--- Nhom3/Models/KetQuaHocTap.cs
+++ Nhom3/Models/KetQuaHocTap.cs
@@ -5,18 +5,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
+//ketquahoctap
 namespace Nhom3.Models
 {
     public class KetQuaHocTap
     {
         [Key]
-        public int MaLHP { get; set; }
+        public int MaLHP { get;  set; }
         [Key]
         public int MaMon { get; set; }
         [Key]
         public int MaSV { get; set; }
 
-        public double DGK { get; set; }
-        public double DCK { get; set; }
+        public double DGK { get; set; } // diem giua ky
+        public double DCK { get; set; }  // diem cuoi ky
     }
 }
